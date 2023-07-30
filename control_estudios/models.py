884ffe0fbd75dca00666a8800097266f931b8016
tomymagno <tomasmagnone@gmail.com>
2023-07-30 18:23:32 -0300

    Agregados de modelo y migracion a la base de datos

--- control_estudios/models.py
+++ control_estudios/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=64)
+    comision = models.IntegerField()
+
+class Estudiante(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    telefono = models.CharField(max_length=20, blank=True)
+    dni = models.CharField(max_length=32)
+    fecha_nacimiento = models.DateField(null=True)
+
+class Profesor(models.Model):
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    dni = models.CharField(max_length=32)
+    email = models.EmailField(blank=True)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    profesion = models.CharField(max_length=128)
+    bio = models.TextField(blank=True)
