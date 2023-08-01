beeb894863bb4e87f59cf1206afa3f6832c43039
tomymagno <tomasmagnone@gmail.com>
2023-07-31 21:55:37 -0300

    Reorganizacion de templates, views y urls

--- sistema_coder/views.py
+++ sistema_coder/views.py
@@ -22,19 +22,3 @@ def saludar_con_html(request):
         )
         return http_response
 
-def listar_estudiantes(request):
-    contexto = {
-        "profesor": "Pedro",
-        "estudiantes": [
-            {"nombre": "Emanuel", "apellido": "Dautel", "nota": 10},
-            {"nombre": "Manuela", "apellido": "Gomez", "nota": 4},
-            {"nombre": "Ivan", "apellido": "Tomasevich", "nota": 6},
-            {"nombre": "Carlos", "apellido": "Perez", "nota": 1}
-        ]
-    }
-    HttpResponse = render(
-        request=request,
-        template_name='base.html',
-        context=contexto,
-    )
-    return HttpResponse
